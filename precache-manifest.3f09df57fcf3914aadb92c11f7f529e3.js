2547801e198d8eb4bf9cc72a7d98c8f9ceeba0a1
Alex Kondratenko <kondratenko.ov.8@gmail.com>
2020-04-28 17:13:36 +0300

    Updates

---
+++ precache-manifest.3f09df57fcf3914aadb92c11f7f529e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79064d42e775c22db5c4ed9c511bacd4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e02a35f946e01ba4819",
+    "url": "/monsters-rolodex/static/css/main.70797cd0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e02a35f946e01ba4819",
+    "url": "/monsters-rolodex/static/js/main.a25d6335.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
